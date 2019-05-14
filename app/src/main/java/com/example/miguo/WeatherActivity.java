ed806ca9066f817e1490a75e8781da2e3fd3438b
name <email>
2019-05-14 16:03:01 +0800

    增加后台自动跟新天气的功能

--- app/src/main/java/com/example/miguo/WeatherActivity.java
+++ app/src/main/java/com/example/miguo/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.miguo;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -59,18 +60,13 @@ public class WeatherActivity extends AppCompatActivity {
                             | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
-        setContentView(R.layout.activity_weather);
+
 
 
 // 初始化各控件
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         navButton = (Button) findViewById(R.id.nav_button);
-        navButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                drawerLayout.openDrawer(GravityCompat.START);
-            }
-        });
+
 
 
         bingPicImg = findViewById(R.id.bing_pic_img);
@@ -116,7 +112,12 @@ public class WeatherActivity extends AppCompatActivity {
             }
         });
 
-
+        navButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
 
 
         String bingPic = prefs.getString("bing_pic", null);
@@ -212,6 +213,8 @@ public class WeatherActivity extends AppCompatActivity {
                         carWashText.setText(carWash);
                         sportText.setText(sport);
                         weatherLayout.setVisibility(View.VISIBLE);
+                        Intent intent = new Intent(this,AutoUpdateService.class);
+                        startService(intent);
                     }
 
     private void loadBingPic() {
